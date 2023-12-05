fc87c9bb610bd58259e9f4e38bf5d3421951021f
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-05 15:54:49 +0000

    Delete src/test/java directory

--- src/test/java/menus/TestBPPcontainerView.java
+++
@@ -1,9 +0,0 @@
-package test.java.menus;
-
-import main.java.concrete.Problems.containerBPP.BPPcontainerProblem;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class TestBPPcontainerView {
-
-}
