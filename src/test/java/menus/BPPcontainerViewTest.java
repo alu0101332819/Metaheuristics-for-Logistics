f08906a81bf9a92d46add9f378e97a5a09208ea0
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-05 17:28:55 +0000

    Update BPPcontainerViewTest.java

--- src/test/java/menus/BPPcontainerViewTest.java
+++ src/test/java/menus/BPPcontainerViewTest.java
@@ -9,6 +9,52 @@ import main.java.concrete.metaheuristics.containerBPP.BPPGreedy;
 import main.java.concrete.metaheuristics.containerBPP.BPPHillClimbing;
 import main.java.concrete.metaheuristics.containerBPP.BPPSimulatedAnnealing;
 
-public class BPPcontainerViewTest {
-    
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BPPcontainerViewTest {
+
+    private BPPcontainerView bppContainerView;
+    private Scanner mockScanner;
+
+    @BeforeEach
+    void setUp() {
+        bppContainerView = new BPPcontainerView();
+        mockScanner = mock(Scanner.class);
+    }
+
+    @Test
+    void testRunExitOption() {
+        // Arrange
+        InputStream in = new ByteArrayInputStream("exit".getBytes());
+        System.setIn(in);
+
+        // Act
+        assertDoesNotThrow(() -> bppContainerView.run(mockScanner, null));
+
+        // Assert
+        verify(mockScanner, times(1)).nextLine();
+    }
+
+    @Test
+    void testRunHillClimbingOption() {
+        // Arrange
+        InputStream in = new ByteArrayInputStream("1\nexit".getBytes());
+        System.setIn(in);
+
+        // Act
+        assertDoesNotThrow(() -> bppContainerView.run(mockScanner, null));
+
+        // Assert
+        verify(mockScanner, times(2)).nextLine();
+        verify(mockScanner, times(1)).nextInt();
+    }
+
+    // Add similar tests for other options (Simulated Annealing, Greedy, Grasp)
 }
