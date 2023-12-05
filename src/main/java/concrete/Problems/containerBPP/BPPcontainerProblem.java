058009fb3571d5f9decd4a4fdfdc5426a4468d9b
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-05 15:07:02 +0000

    Update BPPcontainerProblem.java

--- src/main/java/concrete/Problems/containerBPP/BPPcontainerProblem.java
+++ src/main/java/concrete/Problems/containerBPP/BPPcontainerProblem.java
@@ -9,100 +9,100 @@ import main.java.abstracts.problem.Problem;
 
 public class BPPcontainerProblem extends Problem implements ILoadingProblems {
 
-	private int ContainerArea;
-	private ArrayList<Item> items;
+    private int ContainerArea;
+    private ArrayList<Item> items;
 
-	public BPPcontainerProblem() {
-		// TODO Auto-generated constructor stub
-	}
+    public BPPcontainerProblem() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @param filename Constructor using text file
-	 */
-	public BPPcontainerProblem(String filename) {
-		super();
-		this.textInstances = loadDataFromFile(filename);
-	}
+    /**
+     * @param filename Constructor using text file
+     */
+    public BPPcontainerProblem(String filename) {
+        super();
+        this.textInstances = loadDataFromFile(filename);
+    }
 
-	public BPPcontainerProblem(int capacity, ArrayList<Item> items) {
-		super();
-		this.ContainerArea = capacity;
-		this.items = items;
-	}
+    public BPPcontainerProblem(int capacity, ArrayList<Item> initialItems) {
+        super();
+        this.ContainerArea = capacity;
+        this.items = initialItems;
+    }
 
-	private String loadDataFromFile(String filename) {
-		// Reading the file that contains the instances
-		char[] buff = new char[1024 * 10];
+    private String loadDataFromFile(String filename) {
+        // Reading the file that contains the instances
+        char[] buff = new char[1024 * 10];
 
-		try (FileReader file = new FileReader(filename)) {
-			file.read(buff);
-		} catch (IOException e) {
-			System.out.println("Error. " + e.getMessage());
-			System.exit(0);
-		}
-		return new String(buff);
-	}
+        try (FileReader file = new FileReader(filename)) {
+            file.read(buff);
+        } catch (IOException e) {
+            System.out.println("Error. " + e.getMessage());
+            System.exit(0);
+        }
+        return new String(buff);
+    }
 
-	@Override
-	public void instancesFromText() {
-		String text = textInstances;
-		String[] parts = text.split(":");
-		int capacity = Integer.parseInt(parts[0].trim());
-		ArrayList<Item> items = new ArrayList<>();
-		String[] strItems = parts[1].trim().split(",");
+    @Override
+    public void instancesFromText() {
+        String text = textInstances;
+        String[] parts = text.split(":");
+        int capacity = Integer.parseInt(parts[0].trim());
+        ArrayList<Item> parsedItems = new ArrayList<>();
+        String[] strItems = parts[1].trim().split(",");
 
-		for (String item : strItems) {
-			String[] attributes = item.split("-");
-			items.add(new Item(Integer.parseInt(attributes[0].trim()), Integer.parseInt(attributes[1].trim())));
-		}
+        for (String item : strItems) {
+            String[] attributes = item.split("-");
+            parsedItems.add(new Item(Integer.parseInt(attributes[0].trim()), Integer.parseInt(attributes[1].trim())));
+        }
 
-		this.items = items;
-		this.ContainerArea = capacity;
-	}
+        this.items = parsedItems;
+        this.ContainerArea = capacity;
+    }
 
-	@Override
-	public int getCapacity() {
-		return this.ContainerArea;
-	}
+    @Override
+    public int getCapacity() {
+        return this.ContainerArea;
+    }
 
-	@Override
-	public Item getItem(int index) {
-		return this.items.get(index);
-	}
+    @Override
+    public Item getItem(int index) {
+        return this.items.get(index);
+    }
 
-	@Override
-	public ArrayList<Item> getItems() {
-		return this.items;
-	}
+    @Override
+    public ArrayList<Item> getItems() {
+        return this.items;
+    }
 
-	@Override
-	public int getNumItems() {
-		return this.items.size();
-	}
+    @Override
+    public int getNumItems() {
+        return this.items.size();
+    }
 
-	@Override
-	public String toString() {
-		return "{ CAPACITY = " + this.ContainerArea + " }";
-	}
+    @Override
+    public String toString() {
+        return "{ CAPACITY = " + this.ContainerArea + " }";
+    }
 
-	@Override
-	public double pesoItems() {
-		double p = 0;
+    @Override
+    public double pesoItems() {
+        double p = 0;
 
-		for (Item item : this.items)
-			p += (double) item.getWeight();
+        for (Item item : this.items)
+            p += (double) item.getWeight();
 
-		return p;
-	}
+        return p;
+    }
 
-	@Override
-	public String toString(boolean outputItems) {
-		StringBuilder str = new StringBuilder();
-		if (outputItems) {
-			for (Item i : this.items) {
-				str.append("\n\t").append(i.toString());
-			}
-		}
-		return this.toString() + str;
-	}
+    @Override
+    public String toString(boolean outputItems) {
+        StringBuilder str = new StringBuilder();
+        if (outputItems) {
+            for (Item i : this.items) {
+                str.append("\n\t").append(i.toString());
+            }
+        }
+        return this.toString() + str;
+    }
 }
