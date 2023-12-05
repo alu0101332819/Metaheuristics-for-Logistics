e21abc3a7bb49df132d06e9b2b8706dc75644c85
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-05 17:48:34 +0000

    Update BPPcontainerViewTest.java

--- src/test/java/menus/BPPcontainerViewTest.java
+++ src/test/java/menus/BPPcontainerViewTest.java
@@ -10,36 +10,74 @@ import main.java.concrete.metaheuristics.containerBPP.BPPHillClimbing;
 import main.java.concrete.metaheuristics.containerBPP.BPPSimulatedAnnealing;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class BPPcontainerViewTest {
 
     @Test
-    void testRun() {
-        String input = "BPP_1.txt\n1\nexit";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-
-        Scanner mockScanner = mock(Scanner.class);
-        Mockito.when(mockScanner.nextLine()).thenReturn("BPP_1.txt", "exit");
-
-        // Assuming createBPPcontainerProblem returns a mock of ILoadingProblems for testing
+    void testHillClimbingBPP() {
         ILoadingProblems mockBPPProblem = mock(ILoadingProblems.class);
-        Mockito.when(BPPcontainerView.createBPPcontainerProblem("BPP_1.txt")).thenReturn(mockBPPProblem);
+        BPPcontainerView.hillClimbingBPP(mockBPPProblem);
+        // Add assertions based on the expected behavior of hillClimbingBPP
+        // For example: verify(mockBPPProblem, times(1)).someMethod();
+    }
 
-        // Call the run method
-        BPPcontainerView.run(mockScanner, null);
+    @Test
+    void testSimulatedAnnealingBPP() {
+        ILoadingProblems mockBPPProblem = mock(ILoadingProblems.class);
+        BPPcontainerView.simulatedAnnealingBPP(mockBPPProblem);
+        // Add assertions based on the expected behavior of simulatedAnnealingBPP
+        // For example: verify(mockBPPProblem, times(1)).someMethod();
+    }
 
-        // Add assertions based on the expected behavior
-        // For example, you can check if certain methods were called on mockBPPProblem
-        Mockito.verify(mockBPPProblem, Mockito.times(1)).instancesFromText();
+    @Test
+    void testGreedyBPP() {
+        ILoadingProblems mockBPPProblem = mock(ILoadingProblems.class);
+        BPPcontainerView.greedyBPP(mockBPPProblem);
+        // Add assertions based on the expected behavior of greedyBPP
+        // For example: verify(mockBPPProblem, times(1)).someMethod();
+    }
 
-        // Reset System.in to its original value
-        System.setIn(System.in);
+    @Test
+    void testGraspBPP() {
+        ILoadingProblems mockBPPProblem = mock(ILoadingProblems.class);
+        BPPcontainerView.graspBPP(mockBPPProblem);
+        // Add assertions based on the expected behavior of graspBPP
+        // For example: verify(mockBPPProblem, times(1)).someMethod();
+    }
+
+    @Test
+    void testRunWithDataFile() {
+        String dataFile = "testFile.txt"; // Provide a test file name
+        Scanner mockScanner = mock(Scanner.class);
+        when(mockScanner.nextLine()).thenReturn(dataFile, "exit");
+        InputStream sysInBackup = System.in;
+        try {
+            System.setIn(new ByteArrayInputStream(dataFile.getBytes()));
+            BPPcontainerView.run(mockScanner, null);
+            // Add assertions based on the expected behavior of run
+        } finally {
+            System.setIn(sysInBackup);
+        }
+    }
+
+    @Test
+    void testRunWithNoDataFile() {
+        Scanner mockScanner = mock(Scanner.class);
+        when(mockScanner.nextLine()).thenReturn("exit");
+        InputStream sysInBackup = System.in;
+        try {
+            System.setIn(new ByteArrayInputStream("exit".getBytes()));
+            BPPcontainerView.run(mockScanner, "testFile.txt");
+            // Add assertions based on the expected behavior of run
+        } finally {
+            System.setIn(sysInBackup);
+        }
     }
 }
+
